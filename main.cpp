d3a3cf1c8c45869f345227c376b07c407169be13
varshalohakre <166982714+varshalohakre@users.noreply.github.com>
2024-06-15 00:19:25 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+ int main(){
+    // compile time error
+    std::cout << "hello world" << std::endl;
+//    runtime error
+int value = 7/0;
+std::cout << "value :" << value << std::endl;
+return 0;
+    
+ }
+ 
